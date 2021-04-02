ef06938d61e117f8a15afd5f425dcbe2380ddb8d
Shaik Sajid Ahmed <sajidahmed1002@gmail.com>
2021-04-02 15:43:40 +0530

    Add lists state for todo list

--- src/components/TodoList/index.js
+++ src/components/TodoList/index.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  todo: {
+    width: "40em",
+    margin: "auto",
+    background: "#FFFFFF",
+  },
+  wrapper: {
+    paddingLeft: "10px",
+    fontSize: "1.5em",
+    lineHeight: "2em",
+    display: "inline",
+  },
+});
 
 function TodoList(props) {
-  const { text } = props;
+  const classes = useStyles();
+  const { todos } = props;
+
+  const [lists, setLists] = useState(todos);
 
   return (
-    <>
-      <h2>{text}</h2>
-    </>
+    <div className={classes.todo}>
+      <div className={classes.wrapper}>{lists}</div>
+    </div>
   );
 }
 
