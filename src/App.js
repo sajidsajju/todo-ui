9f9ad6adc1db890c415172e66ca86585234cc78c
Shaik Sajid Ahmed <sajidahmed1002@gmail.com>
2021-04-02 14:37:52 +0530

    Add background color for the container

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
-import { CssBaseline } from "@material-ui/core";
+import { Container, CssBaseline } from "@material-ui/core";
 import TodoList from "./components/TodoList";
 
 const theme = createMuiTheme({
@@ -9,11 +9,18 @@ const theme = createMuiTheme({
   },
 });
 
+const body = `
+body {background: #DFCAA0}
+`;
+
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <TodoList text={"random text"} />
+      <Container>
+        <style>{body}</style>
+        <TodoList text={"random text"} />
+      </Container>
     </ThemeProvider>
   );
 }
