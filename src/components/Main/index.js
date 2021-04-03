c983a69826c832db5573087be4c681f1346e0e9c
Shaik Sajid Ahmed <sajidahmed1002@gmail.com>
2021-04-03 12:03:51 +0530

    Add todo form in main

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { makeStyles } from "@material-ui/core";
 import TodoList from "../TodoList";
+import TodoForm from "../TodoForm";
 
 const useStyles = makeStyles({
   h1: {
@@ -13,11 +14,23 @@ const useStyles = makeStyles({
 
 function Main() {
   const classes = useStyles();
-  const [todos, setTodos] = useState([]);
+  const initialState = JSON.parse(localStorage.getItem("todos")) || [];
+
+  const [todos, setTodos] = useState(initialState);
+
+  const addTodo = (todo) => {
+    const newTodo = [todo, ...todos];
+    setTodos(newTodo);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   return (
     <>
       <h1 className={classes.h1}>TODO</h1>
+      <TodoForm addTodo={addTodo} />
       <TodoList todos={todos} />
     </>
   );
