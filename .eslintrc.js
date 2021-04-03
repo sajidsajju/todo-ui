65e10cfddf2e2e8252e2664c8752c84b2126510c
Shaik Sajid Ahmed <sajidahmed1002@gmail.com>
2021-04-03 13:12:23 +0530

    Add eslint and prettier configuration and rules

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const path = require("path");
+
+const prettierOptions = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, ".prettierrc"), "utf8"),
+);
+
+module.exports = {
+  extends: ["react-app", "react-app/jest", "plugin:prettier/recommended"],
+  rules: {
+    "prettier/prettier": ["error", prettierOptions],
+    "no-console": "error",
+  },
+  overrides: [
+    {
+      files: ["**/*.js"],
+      rules: { "prettier/prettier": ["warn", prettierOptions] },
+    },
+  ],
+};
