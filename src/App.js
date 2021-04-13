68d3bf120e38e2248376e41a87905bfe9e479d73
Shaik Sajid Ahmed <sajidahmed1002@gmail.com>
2021-04-14 01:46:12 +0530

    refactor: remove unused components

--- src/App.js
+++ src/App.js
@@ -10,8 +10,6 @@ import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 import Todo from "./containers/Todo";
 import Login from "./containers/Login";
 import NotFoundPage from "./components/NotFoundPage";
-import { AuthProvider } from "./containers/Login/AuthProvider";
-import PrivateRoute from "./containers/Login/privateRoute";
 
 const theme = createMuiTheme({
   typography: {
@@ -38,15 +36,13 @@ function App() {
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <div className={classes.body}>
-        <AuthProvider>
-          <Router>
-            <Switch>
-              <PrivateRoute exact path="/" component={Todo} />
-              <Route path="/login" component={Login} />
-              <Route component={NotFoundPage} />
-            </Switch>
-          </Router>
-        </AuthProvider>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Todo} />
+            <Route path="/login" component={Login} />
+            <Route component={NotFoundPage} />
+          </Switch>
+        </Router>
       </div>
     </ThemeProvider>
   );
